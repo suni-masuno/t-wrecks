b832d02a031eac28e1fe7449606e8c1109b59ec7
CORPORATE\crb0517 <cassandra.bates@daugherty.com>
2017-09-30 23:31:32 -0500

    question updaaatteeessss

--- src/data/questions.js
+++ src/data/questions.js
@@ -4,17 +4,20 @@
 
 var questions = {
     prompt18: 'Prompt 18 years or older.',
-    regionalPrivacy: 'Display regional privacy policy.',
+    promptLegalGuardian: 'Prompt for legal guardian between ages of 16 and 18.',
+    promptUnder16: 'Do not collect data from individuals under the age of 16.',
     accessInCountryOnly: 'Only allow access within country.',
     accessOutOfCountryAllowed: 'Only allow access out of country if encrypted.',
     informOfThirdParty: 'Inform user of 3rd party information share.',
-    allowDataRemoval: 'Allow user to remove data from system.',
+    allowDataRemoval: 'Allow user to remove data.',
     allowDataModify: 'Allow user to modify data.',
-    requireConsentToProcess: 'Require consent to process personal data.',
-    allowComplaints: 'Allow complaints.',
+    regionalPrivacy: 'Display regional privacy policy.',
+    allowComplaints: 'Allow complaints regarding privacy regulations.',
     requireConsentToCollect: 'Require consent before collecting personal data.',
-    restrictVisitorTracking: 'Restrict visitor tracking via cookies or analytics.',
-    dataResideInCountry: 'Data must reside in the country of origin.',
+    requireInformedConsent: 'Require prior informed consent before collecting personal data.',
+    dataResideInCountry: 'Data must reside in the country of residency.',
+    healthcareRecords: 'Healthcare records can only be viewed in country of residency.',
+    financialRecords: 'Financial records can only be viewed in country of residency.'
 };
 
 export default questions;
